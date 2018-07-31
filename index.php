b95880a9de37f01ce9d66fdb604c538754160960
QuentinDll <quentin.delplace13@gmail.com>
2018-07-31 14:38:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 6</title>
+</head>
+<body>
+  <p>
+    <?php
+    $foot = 20;
+
+    while ($foot >= 0) {
+      echo $foot -- ." C'est presque bon <br />";
+    }
+    ?>
+  </p>
+</body>
+</html>
